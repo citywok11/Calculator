8aacbf0d78607fedfe15ac0581a16b4b5690a797
Behnam <behnam789@hotmail.com>
2023-07-12 20:34:09 +0100

    core features finished needs unit tests now

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@ using Calculator;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.Modal;
-
-
+using Calculator.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ISimpleCalculator, SimpleCalculator>();
 builder.Services.AddBlazoredModal();
 
 
---
+++ Services/SimpleCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator.Services
+{
+    public class SimpleCalculator : ISimpleCalculator
+    {
+        public int Add(int start, int amount)
+        {
+            return start + amount;
+        }
+
+        public int Subtract(int start, int amount)
+        {
+            return start - amount;
+        }
+    }
+}
